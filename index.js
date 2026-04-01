d9ea9c260e38a26bb41a7e6469ee9d1de38dd593
Icey-max-100 <245121733029@mvsrec.edu.in>
2026-04-01 22:47:45 +0530

    Create index.js
    ...

---
+++ index.js
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+<body style="margin:0;background:black">
+
+<video width="100%" height="100%" controls autoplay>
+  <source src="video-twim-icey-1001.mp4" type="video/mp4">
+</video>
+
+</body>
+</html>
